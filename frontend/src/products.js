1075eec7313a8151623f43fb3fe6cc6775b7f4d9
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-02-24 00:57:57 +0100

    Created and styled app components & screens

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Airpods wireless bluetooth headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Est irure aute proident eu sunt in anim sunt nostrud id reprehenderit aliquip.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 3.5,
+    numReviews: 4,
+  },
+  {
+    _id: '2',
+    name: 'alexa',
+    image: '/images/alexa.jpg',
+    description:
+      'Est irure aute proident eu sunt in anim sunt nostrud id reprehenderit aliquip.Proident fugiat cupidatat labore commodo sit mollit voluptate eu enim pariatur nulla labore.Magna ex ex cillum ad eiusmod deserunt sint in laborum magna do.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 135.99,
+    countInStock: 3,
+    rating: 2.3,
+    numReviews: 12,
+  },
+  {
+    _id: '3',
+    name: 'camera',
+    image: '/images/camera.jpg',
+    description:
+      'Est irure aute proideeiusmod deserunt sint in laborum magna do.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 39.99,
+    countInStock: 3,
+    rating: 5,
+    numReviews: 8,
+  },
+  {
+    _id: '4',
+    name: 'mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Est irure aute proident eu sunt in anim sunt nostrud id reprehenderit aliquip.Proident fugiat cupidatat labore commodo sit mollit voluptate eu enim pariatur nulla labore.Magna ex ex cillum ad eiusmod deserunt sint in laborum magna do.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 154.99,
+    countInStock: 3,
+    rating: 4.6,
+    numReviews: 18,
+  },
+  {
+    _id: '5',
+    name: 'phone',
+    image: '/images/phone.jpg',
+    description:
+      'Est irure aute proident eu sunt in anim sunt nostrud id reprehenderit aliquip.Proident fugiat cupidatat labore commodo sit mollit voluptate eu enim pariatur nulla labore.Magna ex ex cillum ad eiusmod deserunt sint in laborum magna do.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 198.99,
+    countInStock: 3,
+    rating: 3.7,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'playstation',
+    image: '/images/playstation.jpg',
+    description:
+      'Est irure aute proident eu sunt in anim sunt nostrud id reprehenderit aliquip.Proident fugiat cupidatat labore commodo sit mollit voluptate eu enim pariatur nulla labore.Magna ex ex cillum ad eiusmod deserunt sint in laborum magna do.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 615.99,
+    countInStock: 3,
+    rating: 4.9,
+    numReviews: 5,
+  },
+];
+
+export default products;
