d4c6163406186d9593c95de45a3e5862ffa4aaba
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-03-02 23:50:24 +0100

    FE auth & profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,16 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser);
 router.post('/login', authUser);
 // to implement middleware we pass as 1st arg
-router.route('/profile').get(protect, getUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
