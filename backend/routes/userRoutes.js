f1c99cc950ac8c136abf9941091301b12402a07b
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-03-02 01:17:37 +0100

    User registration & pw encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
+router.route('/').post(registerUser);
 router.post('/login', authUser);
 // to implement middleware we pass as 1st arg
 router.route('/profile').get(protect, getUserProfile);
