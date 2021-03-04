c456d67036591f85d55d8893ffe788ae1d835ae2
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-03-04 16:30:37 +0100

    Order Screen on logged in user profile

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,7 +16,7 @@ import Message from '../components/Message';
 import { listProductDetails } from '../actions/productActions';
 
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
 
@@ -32,7 +32,7 @@ const ProductScreen = ({ history, match }) => {
   };
 
   return (
-    <div>
+    <>
       <Link className="btn btn-light my-3" to="/">
         Go back
       </Link>
@@ -119,7 +119,7 @@ const ProductScreen = ({ history, match }) => {
           </Col>
         </Row>
       )}
-    </div>
+    </>
   );
 };
 
